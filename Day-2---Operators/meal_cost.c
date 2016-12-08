ead5cb3f4763014d7fe6101a8355b955601ca8b5
ATONU GHOSH <atonughosh@outlook.com>
2016-12-08 08:44:32 +0530

    Create meal_cost.c

---
+++ Day-2---Operators/meal_cost.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    float mealCost,tipPercent,taxPercent,tip,tax,totalCost;
+    int finalCost;
+    scanf("%f",&mealCost);
+    scanf("%f",&tipPercent);
+    scanf("%f",&taxPercent);
+    tip=mealCost*(tipPercent/100);
+    tax=mealCost*(taxPercent/100);
+    totalCost = mealCost + tip + tax;
+    finalCost = round(totalCost);
+    printf("The total meal cost is %d dollars.",finalCost);
+    return 0;
+}
