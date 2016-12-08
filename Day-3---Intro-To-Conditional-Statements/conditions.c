d22b3e0473a050519cb6d9464512db20e5058d33
ATONU GHOSH <atonughosh@outlook.com>
2016-12-08 09:18:39 +0530

    Add files via upload

--- Day-3---Intro-To-Conditional-Statements/conditions.c
+++ Day-3---Intro-To-Conditional-Statements/conditions.c
@@ -1,1 +1,34 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
 
+int main(){
+    int N;
+    scanf("%d",&N);
+    while(N>0 && N<101){
+        if(N%2 != 0){
+            printf("Weird");
+            break;
+        }
+        else
+            {
+            if(N>=2 && N<=5){
+                printf("Not Weird");
+                break;
+            }
+            else if(N>=6 && N<=20){
+                printf("Weird");
+                break;
+            }
+            else if(N>20){
+                printf("Not Weird");
+                break;
+            }
+        }
+    }
+    return 0;
+}
